101621a1d92a0d4cfeb40931fcb65b974f18b4da
Andy-Techy <adjeimitchel03@gmail.com>
2025-11-11 14:19:34 +0000

    made route endpoints for signup , login, refresh , logout and instance

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -6,12 +6,19 @@ const express = require("express");
 const router = express.Router();
 
 //the functions created in the controller
-const { signUp , validator , login } = require("../authController.js");
+const { signUp , validator , login, refresh, logout, instance } = require("../controllers/authController.js");
 
 //then come and make a route for them over here
 //register route
 router.post( "/signup", validator, signUp );
 
 router.post("/login", login );
+
+router.post("/refresh", refresh);
+
+router.post("/logout", logout);
+
+router.post("/instance",  instance);
+
 
 module.exports = router;
