c6ec2c3374142d7b2c73daf757f3d23a82f15a33
Andy-Techy <adjeimitchel03@gmail.com>
2025-11-25 23:31:30 +0000

    modified the vitals file

--- backend/src/models/vitals.js
+++ backend/src/models/vitals.js
@@ -1,20 +1,65 @@
-//src/models/Vital.js
-const mongoose = require('mongoose');
+//src/models/vital.js
+const mongoose = require("mongoose");
 
 const vitalSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User', index: true },
-  timestamp: { type: Date, default: () => new Date(), index: true },
-  heartRate: { type: Number, required: true },
-  systolic: { type: Number, required: true },
-  diastolic: { type: Number, required: true },
-  spo2: { type: Number, required: true },
-  temperatureC: { type: Number, required: true },
-  notes: { type: String, default: '' },
-  source: { type: String, enum: ['web','arduino','simulator','unknown'], default: 'web' },
-  meta: { type: Object, default: {} }
-}, { timestamps: true });
+  userId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    required: true, 
+    ref: 'user', 
+    index: true 
+  },
+
+  heartRate: { 
+    type: Number, 
+    required: true 
+  },
+
+  systolic: { 
+    type: Number, 
+    required: true 
+  },
+
+  diastolic: { 
+    type: Number, 
+    required: true 
+  },
+
+  spo2: { 
+    type: Number, 
+    required: true 
+  },
+  
+  temperature: { 
+    type: Number, 
+    required: true 
+  },
+
+  notes: { 
+    type: String, 
+    default: '' 
+  },
+
+  source: { 
+    type: String, 
+    enum: ['web','arduino','simulator','unknown'], 
+    default: 'web' 
+  },
+
+  meta: { 
+    type: 
+    Object, default: {} 
+  },
+
+  timestamp: { 
+    type: Date, 
+    default: () => new Date(), 
+    index: true 
+  },
+
+}, { timestamps: true }
+);
 
 // Compound index for fast recent queries
 vitalSchema.index({ userId: 1, timestamp: -1 });
 
-module.exports = mongoose.model('Vital', vitalSchema);
+module.exports = mongoose.model("vitals", vitalSchema);
