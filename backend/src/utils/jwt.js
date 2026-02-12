545dbd4d5a85b6d4db72ad10d364f05c154f7ade
Andy-Techy <adjeimitchel03@gmail.com>
2026-02-12 12:33:25 +0000

    this is the utilities files

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -4,15 +4,26 @@ const jwt = require("jsonwebtoken");
 //normal function to get access token
 // a function signAccessToken, takes in the payload and returns a jwt sign of it
 function signAccessToken(payload) {
+
   const secret = process.env.JWT_ACCESS_SECRET;
+
   const expiresIn = process.env.JWT_ACCESS_EXPIRY || '15m';
-  return jwt.sign(payload, secret, { expiresIn });
+
+  return jwt.sign(
+    payload,
+    secret,
+    { expiresIn }
+  );
 }
 
 
 // a function verifyAccessToken takes in the token and verifies using the verify function
 function verifyAccessToken(token) {
-  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+  return jwt.verify(
+    token, 
+    process.env.JWT_ACCESS_SECRET
+  );
 }
 
 //exports both functions
